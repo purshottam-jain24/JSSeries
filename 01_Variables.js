87adc182c503cfa273f674fa8fd38eaf5a6f5ac2
purshottam-jain24 <purshotamjaindelhi11@gmail.com>
2024-04-14 22:23:25 +0530

    01

---
+++ 01_Variables.js
@@ -0,0 +1,86 @@
+/* 
+what is a variable?
+- A variable is a container to store the data temporarily
+- It is used to access the data whenever required.
+
+how to declare a variable?
+- To declare a variable we need to use the keyword "var", "let" or "const".
+- Then we need to give a name to the variable.
+- Then we need to assign a value to the variable.
+
+syntax:
+var variableName = value;
+let variableName = value;
+const variableName = value;
+
+
+what is the difference between var, let and const?
+- var: It is used to declare the variable globally or locally.
+- let: It is used to declare the variable locally.
+- const: It is used to declare the constant variable.
+
+what is the difference between let and const?
+- let: The value of the variable can be changed.
+- const: The value of the variable cannot be changed.
+
+why we shoud use let and const instead of var?
+- The var keyword has function scope.
+- The let keyword has block scope.
+- The const keyword has block scope.
+- The let and const keywords are recommended to use instead of var.
+
+why we stopped using var?
+- The var keyword has function scope and can be hoisted, redeclared and reassigned.
+{for now ignore the hoisting}
+
+what are the rules to declare a variable?
+- The variable name should start with a letter, underscore or dollar sign.
+- The variable name should not start with a number.
+- The variable name should not contain special characters except underscore and dollar sign.
+- The variable name is case-sensitive.
+- The variable name should not be a reserved keyword.
+
+what are the naming conventions for a variable?
+- The variable name should be meaningful.
+- The variable name should be in camelCase.
+- The variable name should not be too long.
+- The variable name should not be too short.
+
+*/
+
+// Example 2: Variables
+
+const UserName = "John Doe";
+let userEmail = "abc@example.com";
+var userpassword = "123456";
+userAge = 25;
+
+console.log(UserName); // Output: John Doe
+console.log(userEmail); // Output: abc@example.com
+console.log(userpassword); // Output: 123456
+console.log(userAge); // Output: 25
+
+// In the above example we have declared the variables UserName, userEmail, userpassword and userAge and assigned the values to them.
+// Then we have printed the values of the variables in the console.
+// Let's try to change value of the variable and see the output.
+
+// Example 3: Change the value of the variable
+UserName = "Jane Prime";
+console.log(UserName); // Output: Jane Prime
+
+userEmail = "xyz@example.com";
+console.log(userEmail); // Output: xyz@example.com
+
+userpassword = "654321";
+console.log(userpassword); // Output: 654321
+
+userAge = 30;
+console.log(userAge); // Output: 30
+
+// In the above example we have changed the values of the variables UserName, userEmail, userpassword and userAge and printed the values in the console.
+// The values of the variables are changed successfully.
+// Now let's try to change the value of the constant variable and see the output.
+
+// But we got *Output: TypeError: Assignment to constant variable.* for the above code. or more specifaclly for UserName = "Jane Prime"; why ?
+// The value of the constant variable cannot be changed. If we try to change the value of the constant variable then it will throw an error.
+// The value of the constant variable is fixed and cannot be changed.
---
+++ basic.js
@@ -0,0 +1,83 @@
+/*
+
+Now what is app.js?
+- app.js is the main javascript file of the application.
+- It is the file where we write our javascript code and logic.
+
+what is .js extension?
+- .js is the extension of javascript files.
+- It is used to identify the javascript files.
+
+why can't we use another extension?
+- We can use another extension but it is a convention to use .js extension for javascript files, because compilers and interpreters identify the file type by its extension.
+
+We can write javascript code in the app.js file and it will be executed in the browser's console or in the runtime environment.
+Now what is runtime environment? and how it is different from the browser's console?
+- Runtime environment is the environment where the code is executed in the termninal or command prompt.
+- Browser's console is the environment where the code is executed in the browser's console.
+
+for example:
+- In the browser's console we can execute the javascript code and see the output in the console.
+- In the runtime environment we can execute the javascript code and see the output in the terminal or command prompt.
+
+Now how to run the javascript code in the runtime environment?
+- To run the javascript code in the runtime environment we need to install node.js in the system.
+- Node.js is a runtime environment for javascript code.
+- It allows us to run the javascript code in the terminal or command prompt.
+
+How to install node.js?
+- To install node.js we need to go to the official website of node.js(https://nodejs.org/en).
+- Then we need to download the node.js installer.
+- After downloading the installer we need to install the node.js in the system.
+- After installing we can run the javascript code in the terminal or command prompt.
+
+How to run the javascript code in the runtime environment?
+- To run the javascript code in the runtime environment we need to create a javascript file with .js extension.
+- Then we need to open the terminal or command prompt.
+- Then we need to navigate to the directory where the javascript file is located.
+- Then we need to run the command "node filename.js" to execute the javascript code.
+- After running the command the javascript code will be executed and the output will be displayed in the terminal or command prompt.
+
+Use code editor to write the javascript code or any other code.
+- Code editor is a software used to write the code.
+- It provides syntax highlighting, code completion, debugging and other features.
+- It helps to write the code easily and efficiently.
+
+Some of the popular code editors are:
+- Visual Studio Code (VS Code) - Developed by Microsoft (Recommended)
+- Sublime Text - Developed by Sublime HQ
+- Atom - Developed by GitHub
+- Notepad++ - Developed by Don Ho
+- Brackets - Developed by Adobe
+- Vim - Developed by Bram Moolenaar
+- Emacs - Developed by Richard Stallman
+
+How to install Visual Studio Code?
+- To install Visual Studio Code we need to go to the official website of Visual Studio Code(https://code.visualstudio.com).
+- Then we need to download the Visual Studio Code installer.
+- After downloading the installer we need to install the Visual Studio Code in the system.
+- After installing we can use the Visual Studio Code to write the code.
+
+How to write the javascript code in the Visual Studio Code?
+- To write the javascript code in the Visual Studio Code we need to open the Visual Studio Code.
+- Then we need to create a new file with .js extension.
+- Then we can write the javascript code in the file.
+- After writing the code we can save the file. (File -> Save) or (Ctrl + S)
+- Then we can run the code in the browser's console or in the runtime environment.
+
+How to run the javascript code in the Visual Studio Code?
+- To run the javascript code in the Visual Studio Code we need to open the terminal in the Visual Studio Code.
+- Then we need to navigate to the directory where the javascript file is located.
+- Then we need to run the command "node filename.js" to execute the javascript code.
+- After running the command the javascript code will be executed and the output will be displayed in the terminal.
+
+Now let's write the javascript code in the app.js file and run it in the browser's console or in the runtime environment.
+
+*/
+
+// Example 1: Hello World Program
+console.log("Hello JavaScript!"); // Output: Hello JavaScript!
+
+// In the above example we have written the javascript code to print the message "Hello JavaScript!" in the console.
+// console.log() is a function used to print the message in the console.
+// "Hello JavaScript!" is the message which we want to print in the console.
